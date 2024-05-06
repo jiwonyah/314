4f069b5d89d06b97341d45927b6cac37c53e1479
yeonjaeim <145633078+yeonjaeim@users.noreply.github.com>
2024-05-07 01:32:44 +0800

    implemented controller for shortlisted page

--- myproject/csit314/controller/favourite/ViewSavedFavouriteController.py
+++ myproject/csit314/controller/favourite/ViewSavedFavouriteController.py
@@ -1,17 +1,12 @@
-from flask import Blueprint, render_template, g, redirect, url_for, request, flash
-from csit314.entity.Review import Review
+from flask import Blueprint, render_template, g
 from csit314.entity.Favourite import Favourite
-from csit314.entity.PropertyListing import PropertyListing
-from csit314.entity.User import User, Role
-from csit314.app import db
-from csit314.controller.authentication.LoginController import login_required
+from sqlalchemy import desc
 
 bp = Blueprint('view_saved_favourite_controller', __name__, template_folder='boundary/templates')
 
-# 즐겨찾기 목록 조회 함수 예시
 @bp.route('/shortlisted')
-@login_required
 def view_my_favourites():
-    favourites = Favourite.query.filter_by(user_userid=g.user.userid).all()
-    favourite_listings = [f.property_listing for f in favourites]
+    # Retrieve the favorites that match the current user's userid, ordered by created_date in descending order.
+    favourites = Favourite.query.filter_by(user_userid=g.user.userid).order_by(desc(Favourite.create_date)).all()
+    favourite_listings = [f.propertyListing for f in favourites]
     return render_template('viewSavedFavouriteBoundary.html', favourite_listings=favourite_listings)
