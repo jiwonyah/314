704ec7abb6b5cd62a99c1437a77169a04df6fd62
yeonjaeim <145633078+yeonjaeim@users.noreply.github.com>
2024-05-12 01:51:06 +0800

    added create_date

--- myproject/csit314/entity/Review.py
+++ myproject/csit314/entity/Review.py
@@ -6,5 +6,6 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author_userid = db.Column(db.String(50), db.ForeignKey('user.userid', ondelete='CASCADE'), nullable=False)
     agent_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     content = db.Column(db.Text, nullable=False)
