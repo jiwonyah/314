1f4119201b622fdd4ed00c27903d65f396df3f73
yeonjaeim <145633078+yeonjaeim@users.noreply.github.com>
2024-05-14 20:55:34 +0800

    added accessibility

--- myproject/csit314/controller/mortgage/BuyerCalculateMortgageController.py
+++ myproject/csit314/controller/mortgage/BuyerCalculateMortgageController.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('calculate_mortgage', __name__, template_folder='boundary/templates')
 
+@buyer_only
 @bp.route('/mortgage')
 def calculateMortgage():
     return render_template('mortgage/calculateMortgage.html')
