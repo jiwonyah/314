bd33ddbf5c1cd16f642a5baf8d86b87e648635ba
yeonjaeim <145633078+yeonjaeim@users.noreply.github.com>
2024-05-14 20:56:51 +0800

    added accessibility

--- myproject/csit314/controller/favourite/ViewSavedFavouriteController.py
+++ myproject/csit314/controller/favourite/ViewSavedFavouriteController.py
@@ -1,14 +1,17 @@
-from flask import Blueprint, render_template, g, jsonify
+from flask import Blueprint, render_template, jsonify
+
+from csit314.controller.role_service.decorators import buyer_only
 from csit314.entity.Favourite import Favourite
 
 bp = Blueprint('view_saved_favourite_controller', __name__, template_folder='boundary/templates')
 
+@buyer_only
 @bp.route('/shortlisted')
 def view_my_favourites_index():
     #favourites = Favourite.query.filter_by(user_userid=g.user.userid).order_by(desc(Favourite.create_date)).all()
     favourites = Favourite.displayShortlist()
     return render_template('favourite/viewSavedFavouriteBoundary.html', favourites=favourites)
-
+@buyer_only
 @bp.route('/api/shortlisted')
 def view_my_favourites():
     # View created_date favorites that match the userid of the current user in descending order
