2798cdbcfb7a5253afdcaf2c11c34ee3577d121a
yeonjaeim <145633078+yeonjaeim@users.noreply.github.com>
2024-05-14 20:56:08 +0800

    added accessibility

--- myproject/csit314/controller/favourite/SaveFavouriteController.py
+++ myproject/csit314/controller/favourite/SaveFavouriteController.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, jsonify, g
 from csit314.app import db
+from csit314.controller.role_service.decorators import buyer_only
 from csit314.entity.Favourite import Favourite
 from sqlalchemy.exc import SQLAlchemyError
 from csit314.controller.favourite.SellerViewSaveCountController import update_shortlist_count
 
 bp = Blueprint('save_favourite_controller', __name__, template_folder='boundary/templates')
 
+@buyer_only
 @bp.route('/toggle_favourite/<int:propertyListing_id>', methods=['POST'])
 def toggle_favourite(propertyListing_id):
     # If g.user is not set, it is considered that the user is not logged in.
