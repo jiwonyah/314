e5238657ff4257ee1ebe380de64cd261682e4777
yeonjaeim <145633078+yeonjaeim@users.noreply.github.com>
2024-05-14 20:53:40 +0800

    added acessibility

--- myproject/csit314/controller/review/AgentViewReviewController.py
+++ myproject/csit314/controller/review/AgentViewReviewController.py
@@ -1,12 +1,28 @@
 from flask import Blueprint, render_template, g, jsonify
 from csit314.entity.Review import Review
-from csit314.entity.User import User
+from csit314.entity.User import User, Role
+from csit314.controller.role_service.decorators import agent_only
 
 bp = Blueprint('view_review_controller', __name__, template_folder='boundary/templates')
 
+@agent_only
 @bp.route('/agent/reviews/')
 def agentViewReviews_index():
-    reviews = Review.query.filter_by(agent_id=g.user.id).all()
+    if not g.user:
+        return jsonify(success=False,
+                       error='Login required to view old property listing'), 401
+
+    elif g.user.role != Role.AGENT:
+        return jsonify(success=False,
+                       error='You are not authorized to view old property listings'), 403  # 권한 없음
+    else:
+        return render_template('review/viewReviewList.html')
+@agent_only
+@bp.route('/api/agent/reviews/')
+def agentViewReviews():
+    # Bring review list from Review entity class
+    reviews = Review.displayReviewsList()
+    #reviews = Review.query.filter_by(agent_id=g.user.id).all()
 
     review_data = []
     for review in reviews:
@@ -14,17 +30,19 @@ def agentViewReviews_index():
         review_data.append({
             'id': review.id,
             'author_name': f"{user.firstName} {user.lastName}",
-            'create_date': review.create_date,
+            'create_date': review.create_date.strftime('%a, %d %b %Y'),
             'rating': review.rating,
             'content': review.content
         })
 
-    return render_template('review/viewReviewList.html', reviews=review_data)
+    return jsonify(reviews=review_data)
 
+@agent_only
 @bp.route('/agent/reviewDetail/<int:review_id>')
 def review_detail_index(review_id):
     review = Review.query.filter_by(id=review_id).first()
     return render_template('review/reviewDetailPage.html', review=review)
+@agent_only
 @bp.route('/api/agent/reviewDetail/<int:review_id>')
 def review_detail(review_id):
     # Return review detail page based on review ID. Return error page if review ID does not exist.
